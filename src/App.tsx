a2a960e5886a8f447b712bd8be943631d5297a92
Henrik Hørlück Berg <henrik@horluck.no>
2019-04-24 10:49:44 +0200

    Remove Firebase, because it doesn't do anything

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import firebase from "firebase/app";
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAJ1b7ZHkkegzVZ0m0tynZXgTuakynFReQ",
-  authDomain: "leirprogram.firebaseapp.com",
-  databaseURL: "https://leirprogram.firebaseio.com",
-  projectId: "leirprogram",
-  storageBucket: "leirprogram.appspot.com",
-  messagingSenderId: "506598439556"
-};
-
-firebase.initializeApp(firebaseConfig);
 
 const App = () => (
       <div className="App">
