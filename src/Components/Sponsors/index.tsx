3f9f0e01dec660ca7f15c30164735da451e056f2
Henrik Hørlück Berg <henrik@horluck.no>
2019-06-17 21:26:01 +0200

    Add basic component for Sponsors

---
+++ src/Components/Sponsors/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+import PanelBase from "nav-frontend-paneler";
+import { Normaltekst, Sidetittel } from "nav-frontend-typografi";
+
+
+export const Sponsors: FC = () => <PanelBase>
+  <Sidetittel>Sponsorer</Sidetittel>
+  <Normaltekst>Kommer så snart alle logoer er sanket inn</Normaltekst>
+</PanelBase>;
