6145f0b0ad7723334ac5d3a8859fcf10488ddd83
Henrik Hørlück Berg <henrik@horluck.no>
2019-06-17 19:01:30 +0200

    Add link to program on homepage

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import { Ingress, Sidetittel } from "nav-frontend-typografi";
 import Lenkepanel from "nav-frontend-lenkepanel/lib";
 import PanelBase from "nav-frontend-paneler";
+import { routes } from "../../App";
 
 
 export const Home = () => <PanelBase>
     <Sidetittel>Velkommen til fylkesleir for Buskerud, Vestfold og Telemark 2019!</Sidetittel>
     <Ingress>
-      På denne siden vil det komme mye spennende info
+      På denne siden vil det komme mye spennende info 🏗
     </Ingress>
-
+  <br />
+  <Lenkepanel tittelProps="sidetittel" href={routes.events} border>Program</Lenkepanel>
+  <br />
   <Lenkepanel tittelProps="sidetittel" href="http://volleyball.fylkesleir.no" border>Volleyball</Lenkepanel>
   </PanelBase>;
