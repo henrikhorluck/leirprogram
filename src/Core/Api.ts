272075325cc47e2d5d6afa044a55ed93e258c592
Henrik Hørlück Berg <henrik@horluck.no>
2019-06-19 16:16:42 +0200

    Fix zero-indexed month and bump a dependency

--- src/Core/Api.ts
+++ src/Core/Api.ts
@@ -29,8 +29,8 @@ export const getEvents = async () =>
         const { startTime, endTime } = event;
         const s = startTime.split(/[^0-9]/).map(el => parseInt(el));
         const e = endTime.split(/[^0-9]/).map(el => parseInt(el));
-        const startDate = new Date(s[0], s[1], s[2], s[3], s[4], s[5]);
-        const endDate = new Date(e[0], e[1], e[2], e[3], e[4], e[5]);
+        const startDate = new Date(s[0], s[1] - 1, s[2], s[3], s[4], s[5]);
+        const endDate = new Date(e[0], e[1] - 1, e[2], e[3], e[4], e[5]);
 
         return {
           ...event,
